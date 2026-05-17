3dce05b7877390163beb198235c72a25b9a0d810
Bartosz Osikowski <bartekosikowski2002@gmail.com>
2026-05-17 21:27:55 +0200

    Every sensors/buttons is working

---
+++ src/main.cpp
@@ -0,0 +1,59 @@
+#include <Arduino.h>
+#include "HX711.h"
+#include <Wire.h>
+#include <Adafruit_PN532.h>
+#include <U8g2lib.h>
+// HX711 circuit wiring
+const int LOADCELL_DOUT_PIN = 1;
+const int LOADCELL_SCK_PIN = 0;
+
+HX711 scale;
+Adafruit_PN532 nfc(-1, -1);
+U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R2);
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin(8, 9);
+  nfc.begin();
+  u8g2.begin();
+  u8g2.setFont(u8g2_font_ncenB14_tr);
+  
+  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+  scale.set_scale(-638.0f);
+  scale.tare();
+
+  pinMode(20, INPUT_PULLUP);
+  pinMode(21, INPUT_PULLUP);
+  
+  
+}
+
+void loop() {
+  //-------buttons
+  if(!digitalRead(20)){
+    Serial.println("Button 1 pressed");
+  }
+  if(!digitalRead(21)){
+    Serial.println("Button 2 pressed");
+  }
+  //-------display
+  u8g2.clearBuffer();
+  u8g2.drawStr(0,20,"Hello World!");
+  u8g2.sendBuffer();
+
+  //-------------- NFC
+  uint8_t success;
+  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 }; 
+  uint8_t uidLength;   
+  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength, 50);
+
+  if(success){
+    Serial.print("UID value:");
+    nfc.PrintHex(uid, uidLength);
+  }
+  //--------------- HX711
+  Serial.println("Odczyty z belki:");
+  Serial.println(scale.get_units(10), 1);
+  delay(10);
+  
+}
